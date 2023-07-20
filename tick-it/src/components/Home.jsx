33b5f17d2af154294829e693465b6256008dae30
Rustam Boura <rustyswag@gmail.com>
2023-07-20 14:16:59 -0400

    updated Sports Theatre and Concerts Event List

--- tick-it/src/components/Home.jsx
+++ tick-it/src/components/Home.jsx
@@ -1,112 +1,13 @@
-// import React, { useState } from 'react';
-// import Dropdown from 'react-bootstrap/Dropdown';
-// import { Link } from 'react-router-dom';
-
-// const Home = () => {
-//   const [open, setOpen] = useState(false);
-
-//   const handleToggle = (event, metadata) => {
-//     if (metadata.source === 'select') {
-//       // Prevent closing the dropdown menu when a dropdown item is clicked
-//       event.preventDefault();
-//     }
-
-//     setOpen(!Open);
-//   };
-
-//   return (
-//      <Dropdown onToggle={handleToggle} show={open}>
-//       <Dropdown.Toggle variant="success" id="dropdown-basic">
-//         All Events
-//       </Dropdown.Toggle>
-
-//       <Dropdown.Menu>
-//         <Dropdown.Item as={Link} to="/concerts">
-//           Concerts
-//         </Dropdown.Item>
-//         <Dropdown.Item as={Link} to="/sports">
-//           Sports
-//         </Dropdown.Item>
-//         <Dropdown.Item as={Link} to="/theatre">
-//           Theatre
-//         </Dropdown.Item>
-//       </Dropdown.Menu>
-//     </Dropdown>
-//   )
-// }
-
-// export default Home
-
-// import React, { useState } from 'react';
-// import Dropdown from 'react-bootstrap/Dropdown';
-// import { Link, Route, Switch } from 'react-router-dom';
-// import AllEventList from './AllEventList'
-// import ConcertsEventList from './ConcertsEventList'
-// import SportsEventsList from './SportsEventList'
-// import TheatreEventList from './TheatreEventList'
-
-// const Home = () => {
-//   const [show, setShow] = useState(false);
-//   const [selectedItem, setSelectedItem] = useState('All Events');
-
-//   const handleToggle = (isOpen, event, metadata) => {
-//     if (metadata?.source === 'select') {
-//       event.preventDefault();
-//     } else {
-//       setShow(isOpen);
-//     }
-//   };
-
-//   const handleSelect = (eventKey, event) => {
-//     event.preventDefault(); // Prevent the default behavior of closing the dropdown
-//     setShow(false);
-//     setSelectedItem(eventKey);
-//     // Handle the selected eventKey
-//     console.log('Selected:', eventKey);
-//   };
-  
-
-//   return (
-//     <div>
-
-//       <Dropdown show={show} onToggle={handleToggle} onSelect={handleSelect}>
-//         <Dropdown.Toggle variant="success" id="dropdown-basic">
-//           {selectedItem}
-//         </Dropdown.Toggle>
-
-//         <Dropdown.Menu>
-//           <Dropdown.Item as={Link} to="/AllEventList" eventKey="All Events">All Events</Dropdown.Item>
-//           <Dropdown.Item as={Link} to="/ConcertsEventList" eventKey="Concerts">Concerts</Dropdown.Item>
-//           <Dropdown.Item as={Link} to="/SportsEventList" eventKey="Sports">Sports</Dropdown.Item>
-//           <Dropdown.Item as={Link} to="/TheatreEventList" eventKey="Theatre">Theatre</Dropdown.Item>
-//         </Dropdown.Menu>
-//       </Dropdown>
-
-//       <Switch>
-//         <Route exact path="/" component={AllEventList} />
-//         <Route path="/concerts" component={ConcertsEventList} />
-//         <Route path="/sports" component={SportsEventsList} />
-//         <Route path="/theatre" component={TheatreEventList} />
-//       </Switch>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-
 import React, { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import { Link, Routes, Route } from 'react-router-dom';
-import AllVenues from './AllVenues';
+import { NavLink, Routes, Route } from 'react-router-dom';
 import ConcertsEventList from './ConcertsEventList';
 import SportsEventsList from './SportsEventList';
 import TheatreEventList from './TheatreEventList';
 
 const Home = () => {
   const [show, setShow] = useState(false);
-  const [selectedItem, setSelectedItem] = useState('All Events');
+  const [selectedItem, setSelectedItem] = useState('');
 
   const handleToggle = (isOpen, event, metadata) => {
     if (metadata?.source === 'select') {
@@ -123,29 +24,39 @@ const Home = () => {
     // Handle the selected eventKey
     console.log('Selected:', eventKey);
   };
-
+  
   return (
     <div>
-      <Dropdown show={show} onToggle={handleToggle} onSelect={handleSelect}>
+     
+      <Dropdown show={show} onToggle={handleToggle} onSelect={handleSelect} >
         <Dropdown.Toggle variant="success" id="dropdown-basic">
           {selectedItem}
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item as={Link} to="/" eventKey="All Venues">
+          <Dropdown.Item tag={NavLink} to="/" eventKey="All Venues">
             All Venues
           </Dropdown.Item>
-          <Dropdown.Item as={Link} to="/concerts" eventKey="Concerts">
+          
+          <Dropdown.Item eventKey="Concerts">
+          <NavLink to='/concerts' >
             Concerts
+          </NavLink>
           </Dropdown.Item>
-          <Dropdown.Item as={Link} to="/sports" eventKey="Sports">
+          
+          <Dropdown.Item eventKey="Sports">
+          <NavLink to='/sports' >
             Sports
+          </NavLink>
           </Dropdown.Item>
-          <Dropdown.Item as={Link} to="/theatre" eventKey="Theatre">
+          <Dropdown.Item eventKey="Theatre">
+          <NavLink to='/theatre' >
             Theatre
+          </NavLink>
           </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
+     
 
       <Routes>
         <Route path="/concerts" element={<ConcertsEventList />} />
@@ -158,4 +69,5 @@ const Home = () => {
 };
 
 export default Home;
+
 
--- tick-it/src/components/SportsEventList.jsx
+++ tick-it/src/components/SportsEventList.jsx
@@ -1,20 +1,88 @@
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import React, { useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { NavLink, Routes, Route } from 'react-router-dom';
+import ConcertsEventList from './ConcertsEventList';
+import TheatreEventList from './TheatreEventList';
+
 
 const SportsEventList = () => {
+  const [show, setShow] = useState(false);
+  const [selectedItem, setSelectedItem] = useState('');
+
+  const handleToggle = (isOpen, event, metadata) => {
+    if (metadata?.source === 'select') {
+      event.preventDefault();
+    } else {
+      setShow(isOpen);
+    }
+  };
+
+  const handleSelect = (eventKey, event) => {
+    event.preventDefault(); // Prevent the default behavior of closing the dropdown
+    setShow(false);
+    setSelectedItem(eventKey);
+    // Handle the selected eventKey
+    console.log('Selected:', eventKey);
+  };
+
     return (
-      <div className='gallery'>
-        <Card className='card' style={{ width: '16rem' }}>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1522158637959-30385a09e0da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />
-          <Card.Body>
-            <Card.Title>Sport Event Title</Card.Title>
-            <Card.Text>
-              Date and Venue
-            </Card.Text>
-            <Button variant="primary">Add To Cart</Button>
-          </Card.Body>
-        </Card>
-      </div>
+      <div>
+         <div className='Dropdown'>
+          <Dropdown show={show} onToggle={handleToggle} onSelect={handleSelect} >
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            {selectedItem}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item eventKey="All Venues">
+              <NavLink to='/'>
+              All Venues
+              </NavLink>
+            </Dropdown.Item>
+            
+            <Dropdown.Item eventKey="Concerts">
+            <NavLink to='/concerts' >
+              Concerts
+            </NavLink>
+            </Dropdown.Item>
+            
+            <Dropdown.Item eventKey="Sports">
+            <NavLink to='/sports' >
+              Sports
+            </NavLink>
+            </Dropdown.Item>
+            <Dropdown.Item eventKey="Theatre">
+            <NavLink to='/theatre' >
+              Theatre
+            </NavLink>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      
+
+        <Routes>
+          <Route path="/concerts" element={<ConcertsEventList />} />
+          <Route path="/sports" element={<SportsEventList />} />
+          <Route path="/theatre" element={<TheatreEventList />} />
+        </Routes>
+
+        </div>
+
+        <div className='gallery'>
+          <Card className='card' style={{ width: '16rem' }}>
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1522158637959-30385a09e0da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />
+            <Card.Body>
+              <Card.Title>Sport Event Title</Card.Title>
+              <Card.Text>
+                Date and Venue
+              </Card.Text>
+              <Button variant="primary">Add To Cart</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        </div>
     )
   }
   
