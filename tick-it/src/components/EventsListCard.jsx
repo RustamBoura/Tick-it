ce5a2b813504894f28c7375019156c6206dfc6a2
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-07-20 10:55:43 -0500

    updated EventListCard

--- tick-it/src/components/EventsListCard.jsx
+++ tick-it/src/components/EventsListCard.jsx
@@ -1,25 +1,51 @@
 import { useNavigate } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const EventCard = (props) => {
+  const [venue, setVenue] = useState({
+    name: 'loading', 
+    events: [],})
+
+
+  const { id } = useParams()
+  useEffect(() => {
+    
+    const selectedVenue = props.allVenues.find((venue) => venue.id == id)
+    setVenue(selectedVenue)
+  }, [venue])
+
   return (
     <div className='container'>
-      <h1>Events at ...</h1>
+      <h1>Events at {venue.name}</h1>
+      <Card>
+        <Card.Img 
+          variant="top" 
+          src={venue.photo_url} 
+        />
+        <Card.Body>
+          <Card.Text>
+            <strong>Location:</strong> {venue.address}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
       <div className='gallery'>
-        {props.allVenues.map((venue) => (
+        {venue.events.map((event) => (
           <Card
-            key={venue.id}
+            key={event.id}
             className='card'
-            style={{ width: '16rem', height: '225px' }}
-            onClick={() => handleVenueClick(venue.id)}
+            style={{ width: '16rem', height: '255px' }}
+            // onClick={() => handleVenueClick(event.id)}
           >
             <Card.Img
               variant='top'
               style={{ maxHeight: '140px', objectFit: 'cover' }}
-              src={venue.photo_url}
+              src={event.photo_url}
             />
             <Card.Body>
-              <Card.Title>{venue.name}</Card.Title>
+              <Card.Title >{event.name}</Card.Title>
             </Card.Body>
           </Card>
         ))}
