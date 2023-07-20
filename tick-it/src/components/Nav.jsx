bd419fb6d677762d9008f7cb75b2c443cb9fc5d6
Kevin Li <kevinli617@gmail.com>
2023-07-20 17:39:50 -0400

    updated navbar links

--- tick-it/src/components/Nav.jsx
+++ tick-it/src/components/Nav.jsx
@@ -1,47 +1,54 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom'
-
-
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-bootstrap";
 
 const CustomNav = () => {
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            {/* <Nav.Link href="#action1">Home</Nav.Link> */}
-            <Link  to='/'>Home</Link>
-            <Link  to='/concerts'>Concerts</Link>
-            <Link  to='/sports'>Sports</Link>
-            <Link  to='/theatre'>Theatre</Link>
-            <Nav.Link href="#action2">My Tickets</Nav.Link>
-            <Nav.Link href="#action2">Sign In</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Container fluid>
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              {/* <Nav.Link href="#action1">Home</Nav.Link> */}
+              <NavLink as={Link} to="/">
+                Home
+              </NavLink>
+              <NavLink as={Link} to="/concerts">
+                Concerts
+              </NavLink>
+              <NavLink as={Link} to="/sports">
+                Sports
+              </NavLink>
+              <NavLink as={Link} to="/theatre">
+                Theatre
+              </NavLink>
+              <Nav.Link href="#action2">My Tickets</Nav.Link>
+              <Nav.Link href="#action2">Sign In</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default CustomNav
+export default CustomNav;
